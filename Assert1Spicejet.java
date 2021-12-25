ad510ab51fa246580fe6d2de8d1adbc0233997ed
Atul Patil <58878642+priyesh069@users.noreply.github.com>
2021-12-25 14:17:49 +0530

    2.1 Assert1Spicejet

---
+++ Assert1Spicejet.java
@@ -0,0 +1,26 @@
+package RahulShettyAca;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Assert1Spicejet
+{
+	public static void main(String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver","G:\\Applicatios\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://spicejet.com/");
+	//	Assert.assertFalse(false);
+		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+		//Assert.assertFalse(true);System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+	}
+}
