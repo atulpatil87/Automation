74329857d980b2892867cc712103a6a36bd7e2ba
Atul Patil <58878642+priyesh069@users.noreply.github.com>
2022-03-31 19:30:47 +0530

    Update 8.GoogleGetIndex.java

--- 8.GoogleGetIndex.java
+++ 8.GoogleGetIndex.java
@@ -8,7 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GoogleGetIndex {
+public class GoogleGetIndex 
+{
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","G:\\Applicatios\\chromedriver_win32\\chromedriver.exe");
