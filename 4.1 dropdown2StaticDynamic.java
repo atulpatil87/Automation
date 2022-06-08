8ea2ff0010782e29c7fc896ee4c108fc661ffcbf
Atul Patil <58878642+priyesh069@users.noreply.github.com>
2022-06-08 19:20:00 +0530

    Update 4.1 dropdown2StaticDynamic.java

--- 4.1 dropdown2StaticDynamic.java
+++ 4.1 dropdown2StaticDynamic.java
@@ -13,7 +13,6 @@ public class dropdown2StaticDynamic
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://spicejet.com"); //URL in the browse
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-
 		driver.findElement(By.xpath("//a[@value='BLR']")).click();
 
 		Thread.sleep(2000);
